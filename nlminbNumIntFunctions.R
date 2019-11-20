f20e8607226a92d7758ba6a45dbec058850e51ba
Henrik Singmann <singmann@gmail.com>
2019-11-20 11:24:14 +0000

    ll is 0, if gamma is 0

--- nlminbNumIntFunctions.R
+++ nlminbNumIntFunctions.R
@@ -9,11 +9,13 @@ int_fun_MK_RNplus <- function(x, pars, radian) {
     
     kc <- sqrt(pars[3]^2 + kappa^2 + 2 * pars[3]*kappa*cos(radian))
     
-    out[i] <- dgamma(kappa, shape = pars[1]/pars[2], scale = pars[2]) * 
-      ((besselI(kc,0,expon.scaled = TRUE) / 
+    out[i] <- dgamma(kappa, shape = pars[1]/pars[2], scale = pars[2]) 
+    if (out[i] != 0) {
+      out[i] <- out[i] * ((besselI(kc,0,expon.scaled = TRUE) / 
           (2*pi*besselI(kappa,0,expon.scaled = TRUE) * 
              besselI(pars[3],0,expon.scaled = TRUE))) *
          exp(kc - (kappa + pars[3])))
+    }
     
   }
   out
